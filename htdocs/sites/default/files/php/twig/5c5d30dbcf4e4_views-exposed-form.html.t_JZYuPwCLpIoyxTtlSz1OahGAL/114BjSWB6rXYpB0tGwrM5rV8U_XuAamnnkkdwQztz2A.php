f30b348ee59ce7fb62c615298b992c851e5649c2
Yura Hromko <yuragromko@gmail.com>
2019-02-08 11:29:11 +0200

    drf

---
+++ htdocs/sites/default/files/php/twig/5c5d30dbcf4e4_views-exposed-form.html.t_JZYuPwCLpIoyxTtlSz1OahGAL/114BjSWB6rXYpB0tGwrM5rV8U_XuAamnnkkdwQztz2A.php
@@ -0,0 +1,88 @@
+<?php
+
+/* core/themes/classy/templates/views/views-exposed-form.html.twig */
+class __TwigTemplate_b57e8699f1f578d604dddb72eee6b4ccf133dd3da1dc0ef3d094a296ffed1174 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $tags = ["if" => 12];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                ['if'],
+                [],
+                []
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 12
+        if ( !twig_test_empty(($context["q"] ?? null))) {
+            // line 13
+            echo "  ";
+            // line 17
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["q"] ?? null), "html", null, true));
+            echo "
+";
+        }
+        // line 19
+        echo "<div class=\"form--inline clearfix\">
+  ";
+        // line 20
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["form"] ?? null), "html", null, true));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/views/views-exposed-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 20,  52 => 19,  47 => 17,  45 => 13,  43 => 12,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "core/themes/classy/templates/views/views-exposed-form.html.twig", "/var/www/drupaltest/htdocs/core/themes/classy/templates/views/views-exposed-form.html.twig");
+    }
+}
